c68bac562356381954a00124a9118940871ace21
Behram <77900065+ikhanbehram@users.noreply.github.com>
2022-06-03 01:23:52 +0500

    setting up boilerplate

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View>
+            <View>
+                <TextInput placeholder="Your Goal" />
+                <Button title="Add Goal" />
+            </View>
+            <View>
+                <Text>Your Goals</Text>
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    appContainer: {
+        padding: 15
+    }
 });
